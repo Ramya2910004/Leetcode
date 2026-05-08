fe914b8da9823c5951ea46fb41520c49d42166ad
Ramya2910004 <peluriramya567@gmail.com>
2026-05-08 14:38:56 +0530

    Time: 1 ms (53.02%) | Memory: 13 MB (12.80%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+        int n = nums2.size();
+        
+        stack<int> s;
+        unordered_map<int, int> mp;
+
+        // Find next greater for every element in nums2
+        for(int i = n - 1; i >= 0; i--) {
+
+            while(!s.empty() && s.top() <= nums2[i]) {
+                s.pop();
+            }
+
+            if(s.empty()) {
+                mp[nums2[i]] = -1;
+            }
+            else {
+                mp[nums2[i]] = s.top();
+            }
+
+            s.push(nums2[i]);
+        }
+
+        // Build answer for nums1
+        vector<int> res;
+
+        for(int i = 0; i < nums1.size(); i++) {
+            res.push_back(mp[nums1[i]]);
+        }
+
+        return res;
+    }
+};
